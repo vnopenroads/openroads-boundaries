be70688a7bcdfaa69b6034ee67dcdbf64861b607
Olaf Veerman <olaf.veerman@gmail.com>
2016-04-12 14:10:29 -0400

    Store simplified geometry when exporting for db Contribute to developmentseed/openroads#320

--- tool/generate-areas.js
+++ tool/generate-areas.js
@@ -4,6 +4,7 @@
 var csv = require('csv');
 var fs = require('fs');
 var _ = require('lodash');
+var geojsonExtent = require('geojson-extent');
 
 // For countries we only add the bbox, not the full geometry
 const country = require('../polygons/or_Country-bbox.json');
@@ -18,16 +19,23 @@ const areas = country.features
       .concat(municipalities.features)
       .concat(barangays.features);
 
-// Map an array with geojson feature objects [{geojson},{geojson}]
-// to: [ [id,geojson,type,name],[id,geojson,type,name] ]
+// Takes an array with geojson feature objects [{geojson},{geojson}]
+// The first map substitutes the full boundary for each area with a
+//   simpler polygon containing only the bounding box.
+// The second map turns it into: [ [id,geojson,type,name],[id,geojson,type,name] ]
+
 var final = _(areas)
-    .map((o) => [
-      o.properties.ID_OR,
-      o,
-      o.properties.TYPE,
-      o.properties.NAME
-    ])
-    .value();
+  .map((area) => _(area)
+    .update('geometry', (a) => geojsonExtent.polygon(area))
+    .value()
+  )
+  .map((o) => [
+    o.properties.ID_OR,
+    o,
+    o.properties.TYPE,
+    o.properties.NAME
+  ])
+  .value();
 
 var csvData = csv.stringify(final);
 var writer = fs.createWriteStream('./admin-boundaries-db.csv');
